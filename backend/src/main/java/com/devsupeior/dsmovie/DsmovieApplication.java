6343028227de142cd593e1a28012bb9470b9fa20
João Otávio da Silva <joao.otavio1225@gmail.com>
2022-03-10 11:08:16 -0300

    Security config

--- backend/src/main/java/com/devsupeior/dsmovie/DsmovieApplication.java
+++ backend/src/main/java/com/devsupeior/dsmovie/DsmovieApplication.java
@@ -1,4 +1,4 @@
-package com.devsuperior.dsmovie;
+package com.devsupeior.dsmovie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/com/devsupeior/dsmovie/DsmovieApplicationTests.java
+++ backend/src/test/java/com/devsupeior/dsmovie/DsmovieApplicationTests.java
@@ -1,4 +1,4 @@
-package com.devsuperior.dsmovie;
+package com.devsupeior.dsmovie;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
